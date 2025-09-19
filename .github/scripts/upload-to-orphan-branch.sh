e2cf300b16220b68cdce8d19e14348618706fe7a
Daiji256 <daiji.szk@gmail.com>
2025-09-19 09:18:45 +0900

    Make upload script executable and add debug output

--- .github/scripts/upload-to-orphan-branch.sh
+++ .github/scripts/upload-to-orphan-branch.sh
@@ -50,6 +50,8 @@ if [[ "$IF_NO_FILES_FOUND" = "error" && ${#files[@]} -eq 0 ]]; then
   exit 1
 fi
 
+echo "${!files[@]}"
+
 git worktree remove -f .upload_orphan_worktree || true
 git worktree add --detach .upload_orphan_worktree
 
